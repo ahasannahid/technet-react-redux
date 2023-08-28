cecdd1418c3efea67e9d36eeb60d03ab53acc97a
ahasannahid <ahasan15-9072@diu.edu.bd>
2023-08-28 15:37:23 +0600

    add to cart and remove from cart

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -1,25 +1,52 @@
 import { IProduct } from "@/types/globalTypes";
 import { createSlice } from "@reduxjs/toolkit";
-import  type {PayloadAction} from "@reduxjs/toolkit";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
-interface Icart{
-    products : IProduct[];
+interface ICart {
+    products: IProduct[];
 }
 
-const initialState : Icart = {
-    products : [],
+const initialState: ICart = {
+    products: [],
 }
 
 const cartSlice = createSlice({
     name: "cart",
     initialState,
-    reducers : {
+    reducers: {
         addToCart: (state, action: PayloadAction<IProduct>) => {
-            state.products.push(action.payload)
+
+            const existing = state.products.find(product => product._id === action.payload._id);
+
+            if (existing) {
+                existing.quantity = existing.quantity! + 1;
+            } else {
+                state.products.push({ ...action.payload, quantity: 1 });
+            }
+
+
         },
+        removeFromCart: (state, action: PayloadAction<IProduct>) => {
+            state.products = state.products.filter(
+                product => product._id !== action.payload._id
+            );
+        },
+        removeOne : (state, action: PayloadAction<IProduct>) => {
+
+            const existing = state.products.find(product => product._id === action.payload._id);
+
+            if (existing && existing.quantity! > 1) {
+                existing.quantity = existing.quantity! - 1;
+            } else {
+                state.products = state.products.filter(
+                    product => product._id !== action.payload._id
+                );
+            }
+        },
+
     },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, removeOne } = cartSlice.actions;
 
 export default cartSlice.reducer;
